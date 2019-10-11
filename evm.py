211dc2ccf295fc422cd17402a388041949f84591
tulasi81 <55742078+tulasi81@users.noreply.github.com>
2019-10-11 13:11:40 +0530

    Create evm.py

---
+++ evm.py
@@ -0,0 +1,20 @@
+from array import *
+voterid=[]
+votername=[]
+voterage=[]
+anand=0
+nani=0
+def candidate1():
+anand+=1
+c1[]=anand
+
+def candidate2():
+nani+=1
+c2[]=nani
+
+def voter():
+voterid.append(int(input("Enter 
+
+def castvote():
+
+  
